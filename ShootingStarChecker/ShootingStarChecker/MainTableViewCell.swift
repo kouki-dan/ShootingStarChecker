ac16f406d6435723d2382570fd8371980a19a58c
Kouki Saito <dan.addr.skd@gmail.com>
2014-07-23 23:01:27 +0900

    Add init data

---
+++ ShootingStarChecker/ShootingStarChecker/MainTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MainTableViewCell.swift
+//  ShootingStarChecker
+//
+//  Created by Kouki Saito on 2014/07/21.
+//  Copyright (c) 2014年 Kouki. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    init(style: UITableViewCellStyle, reuseIdentifier: String) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        // Initialization code
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
