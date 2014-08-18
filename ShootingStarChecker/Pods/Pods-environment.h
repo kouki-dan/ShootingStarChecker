5b88d2eab0e0525330776faf123f0d2f86cf2af1
Kouki Saito <dan.addr.skd@gmail.com>
2014-08-18 21:18:33 +0900

    Add SDWebImage

--- ShootingStarChecker/Pods/Pods-environment.h
+++ ShootingStarChecker/Pods/Pods-environment.h
@@ -66,9 +66,81 @@
 #define COCOAPODS_VERSION_MINOR_QuickDialog_Core 0
 #define COCOAPODS_VERSION_PATCH_QuickDialog_Core 0
 
+// SDWebImage
+#define COCOAPODS_POD_AVAILABLE_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage 1
+
+// SDWebImage/Core
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 1
+
+// SDWebImage/MapKit
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_MapKit
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_MapKit 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_MapKit 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage_MapKit 1
+
+// SDWebImage/WebP
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_WebP
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_WebP 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_WebP 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage_WebP 1
+
 // SVProgressHUD
 #define COCOAPODS_POD_AVAILABLE_SVProgressHUD
 #define COCOAPODS_VERSION_MAJOR_SVProgressHUD 1
 #define COCOAPODS_VERSION_MINOR_SVProgressHUD 0
 #define COCOAPODS_VERSION_PATCH_SVProgressHUD 0
+
+// libwebp
+#define COCOAPODS_POD_AVAILABLE_libwebp
+#define COCOAPODS_VERSION_MAJOR_libwebp 0
+#define COCOAPODS_VERSION_MINOR_libwebp 4
+#define COCOAPODS_VERSION_PATCH_libwebp 1
+
+// libwebp/dec
+#define COCOAPODS_POD_AVAILABLE_libwebp_dec
+#define COCOAPODS_VERSION_MAJOR_libwebp_dec 0
+#define COCOAPODS_VERSION_MINOR_libwebp_dec 4
+#define COCOAPODS_VERSION_PATCH_libwebp_dec 1
+
+// libwebp/demux
+#define COCOAPODS_POD_AVAILABLE_libwebp_demux
+#define COCOAPODS_VERSION_MAJOR_libwebp_demux 0
+#define COCOAPODS_VERSION_MINOR_libwebp_demux 4
+#define COCOAPODS_VERSION_PATCH_libwebp_demux 1
+
+// libwebp/dsp
+#define COCOAPODS_POD_AVAILABLE_libwebp_dsp
+#define COCOAPODS_VERSION_MAJOR_libwebp_dsp 0
+#define COCOAPODS_VERSION_MINOR_libwebp_dsp 4
+#define COCOAPODS_VERSION_PATCH_libwebp_dsp 1
+
+// libwebp/enc
+#define COCOAPODS_POD_AVAILABLE_libwebp_enc
+#define COCOAPODS_VERSION_MAJOR_libwebp_enc 0
+#define COCOAPODS_VERSION_MINOR_libwebp_enc 4
+#define COCOAPODS_VERSION_PATCH_libwebp_enc 1
+
+// libwebp/mux
+#define COCOAPODS_POD_AVAILABLE_libwebp_mux
+#define COCOAPODS_VERSION_MAJOR_libwebp_mux 0
+#define COCOAPODS_VERSION_MINOR_libwebp_mux 4
+#define COCOAPODS_VERSION_PATCH_libwebp_mux 1
+
+// libwebp/utils
+#define COCOAPODS_POD_AVAILABLE_libwebp_utils
+#define COCOAPODS_VERSION_MAJOR_libwebp_utils 0
+#define COCOAPODS_VERSION_MINOR_libwebp_utils 4
+#define COCOAPODS_VERSION_PATCH_libwebp_utils 1
+
+// libwebp/webp
+#define COCOAPODS_POD_AVAILABLE_libwebp_webp
+#define COCOAPODS_VERSION_MAJOR_libwebp_webp 0
+#define COCOAPODS_VERSION_MINOR_libwebp_webp 4
+#define COCOAPODS_VERSION_PATCH_libwebp_webp 1
 
